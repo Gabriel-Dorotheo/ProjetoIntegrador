85afb7d52713217076319b72839ebd66cb41c47b
Gabriel Dorotheo <gbldorotheo@hotmail.com>
2023-05-10 04:43:30 -0300

    atualização da tela usuários

--- usuario.php
+++ usuario.php
@@ -21,14 +21,14 @@
 
 
         body {
-            font-family: 'Kalam', cursive;
-            font-family: 'Noto Sans', sans-serif;
-            font-family: 'Roboto Condensed', sans-serif;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         }
 
         .fundo {
-            background: rgb(175, 255, 226);
-            background: linear-gradient(270deg, rgba(175, 255, 226, 1) 0%, rgba(240, 210, 255, 1) 100%);
+            background: #2980B9;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #FFFFFF, #6DD5FA, #2980B9);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #FFFFFF, #6DD5FA, #2980B9); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
         }
 
         .animar {
@@ -43,16 +43,8 @@
             top: 180px;
         }
 
-        .swal-modal{
-            background-color: white;
-        }
-
-        .swal-text{
-            text-align: center;
-            color: black;
-        }
-        .swal-title{
-            color: black;
+        .sombra-texto {
+            text-shadow: 4px 4px 4px rgba(110, 110, 110, 0.72);
         }
     </style>
 </head>
@@ -64,30 +56,43 @@
 
         <!-- Linha da logo do site -->
 
-        <div class="row border border-bottom border-secondary-subtle bg-white">
+        <div class="row bg-transparent">
             <!-- Coluna da logo -->
-            <div class="col-3 text-center">
-                <logo class="animar fs-1 d-inline-flex mt-1">
+            <div class="col-4 text-center opacity-100">
+                <logo class="animar fs-1 d-inline-flex mt-1 mb-2 sombra-texto">
                     Pharma Flow
                 </logo>
 
             </div>
 
+            <div class="col mt-4 fs-4 d-flex justify-content-center">
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="page1.html">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Usuários</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Produtos</a>
+                    </li>
+                </ul>
+            </div>
 
             <!-- Coluna do icone de menu -->
-            <div class="col-9 d-flex justify-content-end fs-3">
+            <div class="col-4 d-flex justify-content-end fs-5">
                 <nav class="navbar navbar-expand-lg me-5">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
                         aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                        <span class="bi bi-person-circle"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <div class="collapse navbar-collapse me-5" id="navbarNavDropdown">
                         <ul class="navbar-nav">
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle me-5" href="#" role="button"
+                                <a class="nav-link dropdown-toggle" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="bi bi-person-circle"></i>
+                                    <i class="bi bi-person-circle fs-4"></i>
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="#">Configurações</a></li>
@@ -101,80 +106,76 @@
             </div>
         </div>
 
-
         <!-- Menu -->
 
-        <div class="row">
-            <div class="col mt-5 fs-4 d-flex justify-content-center">
-                <ul class="nav nav-underline ">
-                    <li class="nav-item">
-                        <a class="nav-link" href="page1.html">Início</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Usuários</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produtos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Configurações</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
 
 
-        <!-- Menu lateral esquerdo -->
+
         <div class="row">
             <div class="col">
-                <div class="container mt-5 mb-5 border rounded-2 shadow bg-white rounded">
+                <!-- titulo -->
+                <div class="titulo text-center m-5">
+                    <h1 class="h1">Gerenciamento de contas</h1>
+                </div>
+
+                <div class="container mt-5 mb-5 border border-0 rounded-2 shadow bg-white">
+
+                    <!-- botões de adicionar, remover, alterar -->
                     <div class="row">
+                        <div class="col p-3 mt-5 mb-0 d-flex justify-content-end me-5">
 
+                            <button type="button" class="btn btn-success btn-sm me-3" data-bs-toggle="modal"
+                                data-bs-target="#staticBackdrop"><i class="bi bi-person-plus fs-5"></i></button>
 
-                        <div class="titulo text-center m-5">
-                            <h1 class="h1">Gerenciamento de contas</h1>
+                            <button type="button" class="btn btn-primary btn-sm fs-5 me-3"><i
+                                    class="bi bi-pencil-square"></i></button>
+
+                            <button type="button" class="btn btn-danger btn-sm fs-5" onclick="removerConta()"><i
+                                    class="bi bi-trash3"></i></button>
                         </div>
+                    </div>
 
 
-                        <div class="col-2 p-4 fs-5">
-                            <ul class="list-group list-group-flush border border-0">
-                                <a href="#" class="list-group-item list-group-item-action" data-bs-toggle="modal"
-                                    data-bs-target="#staticBackdrop">Novo
-                                    Usuário</a>
-                            </ul>
-                        </div>
-                        <!-- Conteúdo da tabela -->
+                    <!-- Conteúdo da tabela -->
+                    <div class="row">
                         <div class="col mb-5">
-                            <div class="table-responsive">
+                            <div class="table-responsive ms-5 me-5">
                                 <table class="table">
                                     <thead>
                                         <tr>
-                                            <th scope="col">#</th>
+
+                                            <th scope="col">
+                                            <div class="form-check">
+                                                    <input class="form-check-input border border-primary" type="checkbox" value="checked"
+                                                        id="flexCheckDefault">
+                                                    <label class="form-check-label" for="flexCheckDefault">
+                                                        #
+                                                    </label>
+                                                </div></th>
                                             <th scope="col">Nome</th>
                                             <th scope="col">Sobrenome</th>
                                             <th scope="col">Email</th>
                                             <th scope="col">Usuário</th>
                                             <th scope="col">Senha</th>
-                                            <th scioe="col">Editar</th>
                                         </tr>
                                     </thead>
                                     <tbody class="table-group-divider">
-                                        <?php for ($i = 0; $i < 10; $i++) { ?>
+                                        <?php for ($i = 0; $i < 15; $i++) { ?>
                                             <tr>
                                                 <th scope="row">
-                                                    <?= $i + 1 ?>
+                                                <div class="form-check">
+                                                    <input class="form-check-input border border-primary d-flex" type="checkbox" value=""
+                                                        id="flexCheckDefault">
+                                                    <label class="form-check-label" for="flexCheckDefault">
+                                                        <?= $i + 1 ?>                                      
+                                                    </label>
+                                                </div>
                                                 </th>
                                                 <td>Lorem</td>
                                                 <td>Ipsum</td>
                                                 <td>Lorem@Ipsum.com</td>
                                                 <td>Lorem</td>
                                                 <td>ipsum@1234</td>
-                                                <td class="d-flex justify-content-around">
-                                                    <button type="button" class="btn btn-primary btn-sm"><i
-                                                            class="bi bi-pencil-square"></i></button>
-                                                    <button type="button" class="btn btn-danger btn-sm "
-                                                        onclick="removerConta()"><i class="bi bi-trash3"></i></button>
-                                                </td>
                                             </tr>
 
                                         <?php } ?>
@@ -206,42 +207,43 @@
     <div class="modal fade posicaoModal" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
         tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
-            <div class="modal-content fundo">
+            <div class="modal-content bg-light bg-gradient">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="staticBackdropLabel">Novo Usuário</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body mt-3">
-                    <form class="row g-3">
+                    <form class="row g-3" action="forms/recebe.php" method="POST">
+
                         <div class="col-md-6">
                             <label for="nome" class="form-label">Nome</label>
-                            <input type="email" class="form-control" id="nome">
+                            <input type="text" class="form-control" name="nome" id="nome">
                         </div>
                         <div class="col-md-6">
                             <label for="sobrenome" class="form-label">Sobrenome</label>
-                            <input type="password" class="form-control" id="sobrenome">
+                            <input type="text" class="form-control" name="sobrenome" id="sobrenome">
                         </div>
                         <div class="col-12">
                             <label for="email" class="form-label">Email</label>
-                            <input type="text" class="form-control" id="email" placeholder="">
+                            <input type="email" class="form-control" name="email"id="email" placeholder="">
                         </div>
                         <div class="col-12">
                             <label for="inputAddress2" class="form-label">Usuário</label>
-                            <input type="text" class="form-control" id="inputAddress2" placeholder="">
+                            <input type="text" class="form-control" name="usuario" id="inputAddress2" placeholder="">
                         </div>
                         <div class="col-md-12">
                             <label for="senha" class="form-label">Senha</label>
-                            <input type="text" class="form-control" id="senha">
+                            <input type="password" class="form-control" name="senha" id="senha">
                         </div>
                         <div class="col-md-12">
                             <label for="repetirSenha" class="form-label">Repetir Senha</label>
-                            <input type="text" class="form-control" id="repetirSenha">
+                            <input type="password" class="form-control" name="repetirSenha" id="repetirSenha">
+                        </div>
+                        <div>
+                            <input type="submit" class="btn btn-primary" value="Criar">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
                         </div>
                     </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                    <button type="button" class="btn btn-primary" onclick="criarConta()">Criar</button>
                 </div>
             </div>
         </div>
